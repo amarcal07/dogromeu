6cd7ed667cf44c0161c1ba148d0e4a7f5bfc5eeb
Andresa Marçal <andresamarques3107@gmail.com>
2023-05-11 21:19:10 -0300

    Update

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className='titulo'>Olá, sou o Romeu!</h1>
+        <img className='image' src='src/assets/imagem/Romeu.jpeg' alt='foto'/>
+    </>
+    )
+}
+
+export default Home;
